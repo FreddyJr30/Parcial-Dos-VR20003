39bb85d43fbe3473c3f119b0b15fc9a8c2870d35
Freddy Anastasio Villatoro Ramirez <freddyvillatoro028@gmail.com>
2025-10-02 12:05:49 -0600

    actualizaciones en el ejercicio 1

--- Ejercicio 1/index.php
+++ Ejercicio 1/index.php
@@ -9,10 +9,19 @@
 <body>
     <h1>CAJA DE CREDITO</h1>
     <div class="tabla-amortizacion">
-        
+        <table>
+            <thead>
+                <th>MES</th>
+                <th>CUOTA</th>
+                <th>INTERESES</th> 
+                <th>CAPITAL</th>
+                <th>SALDO PENDIENTE</th>
+            </thead>
+        </table>
     </div>
     <div class="form-solicitud">
         <h3>Ficha de Solicitud de Credito</h3>
+        input
     </div>
 </body>
 </html>
